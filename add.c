60583ddf37b86f94140f0f6cc7dd9ffac6ce76f4
hyh <hyhui666@163.com>
2020-08-06 18:36:10 +0800

    it is master add.c

--- add.c
+++ add.c
@@ -2,16 +2,10 @@
 
 int main(int argc, const char *argv[])
 {
-<<<<<<< HEAD
 	printf("33333333333333333\n");
-
-
-	return 0;
-=======
 
 	printf("2222222222222\n");
 	int a = 9;
 	int b = 8;
 	return a+b;
->>>>>>> hyh
 }
